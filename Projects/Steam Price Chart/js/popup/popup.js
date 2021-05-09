df2cc8c116eccbc1fa3fdb172c176d8707b6f9fc
MaxWang96 <wangzhaodong2018@gmail.com>
2021-05-09 16:33:18 -0500

    During making a toggle switch

--- Projects/Steam Price Chart/js/popup/popup.js
+++ Projects/Steam Price Chart/js/popup/popup.js
@@ -1,25 +1,25 @@
 'use strict';
 
-const swit = document.getElementsByClassName('js-switch')[0];
+// const swit = document.getElementsByClassName('js-switch')[0];
 
-chrome.tabs.query({
-  active: true,
-  currentWindow: true,
-}, (tabs) => {
-  const type = tabs[0].url.split('/')[3];
-  const key = (type === 'app') ? 'appSimp' : 'bundleSimp';
-  chrome.storage.sync.get(key, (value) => {
-    swit.checked = value[key];
-    const initSpeed = swit.checked ? '0s' : '0.4s';
-    const init = new Switchery(swit, {
-      size: 'small',
-      color: '#377096',
-      speed: initSpeed,
-      boxShadow: null,
-    });
-    if (swit.checked) init.options.speed = '0.4s';
-  });
-});
+// chrome.tabs.query({
+//   active: true,
+//   currentWindow: true,
+// }, (tabs) => {
+//   const type = tabs[0].url.split('/')[3];
+//   const key = (type === 'app') ? 'appSimp' : 'bundleSimp';
+//   chrome.storage.sync.get(key, (value) => {
+//     swit.checked = value[key];
+//     const initSpeed = swit.checked ? '0s' : '0.4s';
+//     const init = new Switchery(swit, {
+//       size: 'small',
+//       color: '#377096',
+//       speed: initSpeed,
+//       boxShadow: null,
+//     });
+//     if (swit.checked) init.options.speed = '0.4s';
+//   });
+// });
 
 const lang = window.navigator.languages[0];
 document.body.style['min-width'] = lang.startsWith('zh') ? '120px' : '150px';
@@ -29,33 +29,33 @@ document.getElementById('feedback-btn').onclick = () => {
   window.open('https://chrome.google.com/webstore/detail/stayfocusd/laankejkbhbdhmipfmgcngdelahlfoji/support');
 };
 
-function saveOptions(type) {
-  if (type === 'app') {
-    chrome.storage.sync.set({
-      appSimp: swit.checked,
-    });
-  } else {
-    chrome.storage.sync.set({
-      bundleSimp: swit.checked,
-    });
-  }
-}
+// function saveOptions(type) {
+//   if (type === 'app') {
+//     chrome.storage.sync.set({
+//       appSimp: swit.checked,
+//     });
+//   } else {
+//     chrome.storage.sync.set({
+//       bundleSimp: swit.checked,
+//     });
+//   }
+// }
 
-function changeChart(id) {
-  chrome.tabs.sendMessage(id, {
-    simp: swit.checked,
-  });
-}
+// function changeChart(id) {
+//   chrome.tabs.sendMessage(id, {
+//     simp: swit.checked,
+//   });
+// }
 
-function funcs() {
-  chrome.tabs.query({
-    active: true,
-    currentWindow: true,
-  }, (tabs) => {
-    const type = tabs[0].url.split('/')[3];
-    saveOptions(type);
-    changeChart(tabs[0].id);
-  });
-}
+// function funcs() {
+//   chrome.tabs.query({
+//     active: true,
+//     currentWindow: true,
+//   }, (tabs) => {
+//     const type = tabs[0].url.split('/')[3];
+//     saveOptions(type);
+//     changeChart(tabs[0].id);
+//   });
+// }
 
-document.addEventListener('change', funcs);
+// document.addEventListener('change', funcs);
